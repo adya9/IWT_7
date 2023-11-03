4a6302c4523f2d6ad4518933744c657194e41c87
Adya <72134342+adya9@users.noreply.github.com>
2023-11-03 11:31:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page</title>
+        <link rel="stylesheet" href="register.css">
+    </head>
+    <body>
+        <h1> Register as ADMIN</h1>
+        <a href="register_admin.php"><button class="btn">REGISTER</button></a>
+        <h4>Already have Account <a href="login_admin.php">Login</a></h4>
+        
+
+        <h1> Register as EMPLOYEE</h1>
+        <a href="register_emp.php"><button class="btn">REGISTER</button></a>
+        <h4>Already have Account <a href="login_emp.php">Login</a></h4>
+    </body>
+
+</html>
